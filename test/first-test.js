251d5a26c696f15935d5c913407023f225263592
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-15 16:06:59 +0200

    console

--- test/first-test.js
+++ test/first-test.js
@@ -4,34 +4,33 @@ var webdriver = require("selenium-webdriver");
 describe("testing javascript in the browser", function() {
     beforeEach(function() {
         if (process.env.SAUCE_USERNAME != undefined) {
-            console.log("if");
             this.browser = new webdriver.Builder()
                 .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
                 .withCapabilities({
-                   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
                     build: process.env.TRAVIS_BUILD_NUMBER,
-                    username: "Maartenconnect",
-                    accessKey: "4fd375a2-a635-42fb-860e-346e4d6691a6",
+                    username: process.env.SAUCE_USERNAME,
+                    accessKey: process.env.SAUCE_ACCESS_KEY,
                     browserName: "chrome"
                 }).build();
         } else {
-            console.log("else");
             this.browser = new webdriver.Builder()
                 .withCapabilities({
                     browserName: "chrome"
                 }).build();
         }
 
-        return this.browser.get("http://localhost:4445/travisprojecttest/Website/index.html");
+        return this.browser.get("http://localhost:8000/website/index.html");
     });
 
     afterEach(function() {
         return this.browser.quit();
     });
 
-    it("text h1 should be equal to awesome", function(done) {
+    it("should handle clicking on a headline", function(done) {
         var headline = this.browser.findElement(webdriver.By.css('h1'));
 
+        headline.click();
 
         headline.getText().then(function(txt) {
             assert.equal(txt, "awesome");
