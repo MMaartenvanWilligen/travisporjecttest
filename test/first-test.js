48afbb547aa584a38ad85c1a09ea198dba887ba4
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-15 16:34:10 +0200

    test update

--- test/first-test.js
+++ test/first-test.js
@@ -1,11 +1,11 @@
 var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 
-describe("testing javascript in the browser", function() {
-    beforeEach(function() {
+describe("testing javascript in the browser", function () {
+    beforeEach(function () {
         if (process.env.SAUCE_USERNAME != undefined) {
             this.browser = new webdriver.Builder()
-                .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+                .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
                 .withCapabilities({
                     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
                     build: process.env.TRAVIS_BUILD_NUMBER,
@@ -23,11 +23,11 @@ describe("testing javascript in the browser", function() {
         return this.browser.get("http://localhost:8000/website/index.html");
     });
 
-    afterEach(function() {
+    afterEach(function () {
         return this.browser.quit();
     });
 
-    describe("testing if text is set", function() {
+    describe("text set", function () {
 
         it("h1 shuold be awesome", function (done) {
             var headline = this.browser.findElement(webdriver.By.css('h1'));
@@ -42,14 +42,22 @@ describe("testing javascript in the browser", function() {
 
 
         it("CTA button should be raised", function (done) {
-            var button =  this.browser.findElement(webdriver.by.css('button'));
+            var button = this.browser.findElement(webdriver.by.css('button'));
 
-            button.getClass().then(function(classs) {
+            button.getClass().then(function (classs) {
                 assert.equal(classs, "raised");
-            };
+            });
             done();
 
         });
+
+    });
+
+    describe('Home page', function () {
+        it('should load the page properly');
+        it('should navigate to login');
+        it('should navigate to sign up');
+        it('should load analytics');
     });
 
 });
