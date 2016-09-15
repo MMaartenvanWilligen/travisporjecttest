257f6e78951725da0a8417de6054c06cc76c8c8c
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-15 15:15:37 +0200

    console

--- test/first-test.js
+++ test/first-test.js
@@ -4,16 +4,18 @@ var webdriver = require("selenium-webdriver");
 describe("testing javascript in the browser", function() {
     beforeEach(function() {
         if (process.env.SAUCE_USERNAME != undefined) {
+            console.log("if");
             this.browser = new webdriver.Builder()
                 .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
                 .withCapabilities({
-                    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
                     build: process.env.TRAVIS_BUILD_NUMBER,
                     username: "Maartenconnect",
                     accessKey: "4fd375a2-a635-42fb-860e-346e4d6691a6",
                     browserName: "chrome"
                 }).build();
         } else {
+            console.log("else");
             this.browser = new webdriver.Builder()
                 .withCapabilities({
                     browserName: "chrome"
