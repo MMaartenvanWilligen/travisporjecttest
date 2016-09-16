3a60f904009e67e3e4598285708ae55a06934c74
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-16 14:54:33 +0200

    chai should 5

--- test/buttont.js
+++ test/buttont.js
@@ -1,27 +1,3 @@
 
 
-    it("CTA button should be raised", function (done) {
-
-        var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
-
-        button.getText().then(function (txt) {
-            assert.equal(txt, "RAISED");
-            done();
-        });
-
-
-    });
-
-    it("expect onclick text change to buttontransform", function (done) {
-
-        var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
-
-        button.click();
-
-        button.getText().then(function (txt) {
-            assert.equal(txt, "BUTTONTRANSFORM");
-            done();
-        });
-
-    });
 
