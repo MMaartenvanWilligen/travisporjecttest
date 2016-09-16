28303cf164e43e9cc31d273a129ec76d199906cb
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-16 15:12:41 +0200

    require other test

--- test/first-test.js
+++ test/first-test.js
@@ -1,8 +1,8 @@
 var assert = require("assert");
 var webdriver = require("selenium-webdriver");
 var chai = require('chai')
-     , expect = chai.expect
-     , should = chai.should();
+    , expect = chai.expect
+    , should = chai.should();
 
 describe("testing javascript in the browser", function () {
 
@@ -87,30 +87,7 @@ describe("testing javascript in the browser", function () {
 
     describe("buttons", function () {
 
-        it("CTA button should be raised", function (done) {
-
-            var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
-
-            button.getText().then(function (txt) {
-                assert.equal(txt, "RAISED");
-                done();
-            });
-
-
-        });
-
-        it("expect onclick text change to buttontransform", function (done) {
-
-            var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
-
-            button.click();
-
-            button.getText().then(function (txt) {
-                assert.equal(txt, "BUTTONTRANSFORM");
-                done();
-            });
-
-        });
+        require("./buttont");
 
 
     });
