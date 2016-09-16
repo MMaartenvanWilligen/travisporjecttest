b1194c7046eb908f338e09df903c40693cfffa04
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-16 16:09:24 +0200

    file ordering

--- test/first-test.js
+++ test/first-test.js
@@ -87,7 +87,31 @@ describe("testing javascript in the browser", function () {
 
     describe("buttons", function () {
 
-      require("./buttont");
+        it("CTA button should be raised", function (done) {
+
+            var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
+
+            button.getText().then(function (txt) {
+                assert.equal(txt, "RAISED");
+                done();
+            });
+
+
+        });
+
+        it("expect onclick text change to buttontransform", function (done) {
+
+            var button = this.browser.findElement(webdriver.By.id("raisedbutton"));
+
+            button.click();
+
+            button.getText().then(function (txt) {
+                assert.equal(txt, "BUTTONTRANSFORM");
+                done();
+            });
+
+        });
+
 
 
     });
