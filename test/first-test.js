d644caf7af27defca81bc7ace740d50323fe6023
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-16 15:32:56 +0200

    require other test2

--- test/first-test.js
+++ test/first-test.js
@@ -4,93 +4,114 @@ var chai = require('chai')
     , expect = chai.expect
     , should = chai.should();
 
-describe("testing javascript in the browser", function () {
 
-    beforeEach(function () {
-        if (process.env.SAUCE_USERNAME != undefined) {
-            this.browser = new webdriver.Builder()
-                .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
-                .withCapabilities({
-                    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-                    build: process.env.TRAVIS_BUILD_NUMBER,
-                    username: process.env.SAUCE_USERNAME,
-                    accessKey: process.env.SAUCE_ACCESS_KEY,
-                    browserName: "chrome"
-                }).build();
-        } else {
-            this.browser = new webdriver.Builder()
-                .withCapabilities({
-                    browserName: "chrome"
-                }).build();
-        }
+init();
 
-        return this.browser.get("http://localhost:8000/website/index.html");
-    });
+function init() {
 
-    afterEach(function () {
-        return this.browser.quit();
-    });
+    setTunnel();
+}
 
-    describe("text set", function () {
+function setTunnel() {
 
-        it("h1 should be awesome", function (done) {
-            var headline = this.browser.findElement(webdriver.By.css('h1'));
+    if (process.env.SAUCE_USERNAME != undefined) {
+        this.browser = new webdriver.Builder()
+            .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
+            .withCapabilities({
+                'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                build: process.env.TRAVIS_BUILD_NUMBER,
+                username: process.env.SAUCE_USERNAME,
+                accessKey: process.env.SAUCE_ACCESS_KEY,
+                browserName: "chrome"
+            }).build();
+    } else {
+        this.browser = new webdriver.Builder()
+            .withCapabilities({
+                browserName: "chrome"
+            }).build();
+    }
+
+    this.browser.get("http://localhost:8000/website/index.html");
+
+    test();
+}
+
+function test() {
 
 
-            headline.getText().then(function (txt) {
-                assert.equal(txt, "awesome");
-                done();
+    describe("testing javascript in the browser", function () {
+
+
+
+        describe("text set", function () {
+
+            it("h1 should be awesome", function (done) {
+                var headline = this.browser.findElement(webdriver.By.css('h1'));
+
+
+                headline.getText().then(function (txt) {
+                    assert.equal(txt, "awesome");
+                    done();
+                });
             });
         });
-    });
 
-    describe('Home page', function () {
+        describe('Home page', function () {
 
-        it('should load the page properly', function () {
-            assert.ok(true);
+            it('should load the page properly', function () {
+                assert.ok(true);
+            });
+
+
+            it('should navigate to login', function () {
+
+
+            });
+
+
+            it('should navigate to contact');
+            it('should load analytics');
         });
 
-
-        it('should navigate to login', function () {
-
-
-        });
-
-
-        it('should navigate to contact');
-        it('should load analytics');
-    });
-
-    describe('Images', function () {
-        it('should be visible in the screen');
-        it('should be more then 0px height and width');
-        it('should have rel');
-
-    });
-
-    describe('Form', function () {
-
-
-        describe("before submit", function () {
-
+        describe('Images', function () {
+            it('should be visible in the screen');
+            it('should be more then 0px height and width');
+            it('should have rel');
 
         });
 
-        describe("test after submit ", function () {
-            it("expect status 200 after submit");
-            it("should return error for false email");
+        describe('Form', function () {
+
+
+            describe("before submit", function () {
+
+
+            });
+
+            describe("test after submit ", function () {
+                it("expect status 200 after submit");
+                it("should return error for false email");
+
+            });
+
+
+        });
+
+        describe("buttons", function () {
+
+            require("./buttont");
+
 
         });
 
 
     });
 
-    describe("buttons", function () {
+    quitBrowser();
 
-        require("./buttont");
+}
 
+function quitBrowser() {
 
-    });
-
-
-});
+    this.browser.quit();
+}
