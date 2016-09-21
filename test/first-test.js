d3dd5e3a5134585ec5bfd134ba8beefe7807339c
Maarten van Willigen <maarten@rik-ws.dhcpdns>
2016-09-21 09:46:39 +0200

    add first-test file

--- test/first-test.js
+++ test/first-test.js
@@ -52,7 +52,6 @@ describe("testing javascript in the browser", function () {
 
     describe('Home page', function () {
 
-
         console.log('HompageObject.constructor is ' + HomepageObject.constructor);
 
         it('should load the page properly', function () {
