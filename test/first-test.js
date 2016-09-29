438596c981bd6c3b796426aa8753a2602b7fccd2
Maarten van Willigen <maarten@connectholland.nl>
2016-09-29 15:39:54 +0200

    add pages 17

--- test/first-test.js
+++ test/first-test.js
@@ -9,14 +9,12 @@ var Login = require("./page-objects/loginPage");
 var Contact = require("./page-objects/contactPage");
 
 
-
-
 describe("testing javascript in the browser", function () {
 
 
-/*
-* declaring variables
-*/
+    /*
+     * declaring variables
+     */
     var driver;
     var homepage;
     var loginPage;
@@ -38,76 +36,45 @@ describe("testing javascript in the browser", function () {
          * ready for testing
          * */
 
+        driver = require("./driver").GetDriver();
+        console.log("driver is" + " " + driver);
 
-            driver = require("./driver").GetDriver();
-            console.log("driver is" + " " + driver);
-
-            homepage = new Home(driver);
-            homepage.getUrl();
-
-
-        it("h1 text should be awesome", function (done) {
-            homepage.header().getText().then(function (txt) {
-                assert.equal(txt, "awesome");
-                done();
-            });
-        });
-
-        it('should load the page properly', function () {
-
-        });
-
-        describe('Images', function () {
-            it('should be visible in the screen');
-            it('should be more then 0px height and width');
-            it('should have rel');
-        });
-
-        describe('Form', function () {
-
-            describe("before submit", function () {
-
-            });
-
-            describe("test after submit ", function () {
-                it("expect status 200 after submit");
-                it("should return error for false email");
-            });
-        });
+        homepage = new Home(driver);
+        homepage.getUrl();
 
         describe("buttons", function () {
 
-            it("CTA button should be raised", function (done) {
+            it("h1 text should be awesome", function (done) {
+                homepage.header().getText().then(function (txt) {
+                    assert.equal(txt, "awesome");
+                    done();
+                });
+            });
 
+            it("CTA button should be raised", function (done) {
                 var button = homepage.ctaButton();
                 console.log("button" + " " + button);
                 button.getText().then(function (txt) {
                     assert.equal(txt, "RAISED");
                     done();
                 });
-
             });
 
             it("Expect onclick text change to buttontransform", function (done) {
-
                 homepage.ctaButtonClick();
                 homepage.ctaButton().getText().then(function (txt) {
                     assert.equal(txt, "BUTTONTRANSFORM");
                     done();
                 });
-
             });
-
-
         });
-
     });
 
-    describe("inlogPagina", function () {
+    describe("Login page", function () {
 
 
-            loginPage = new Login(driver);
-            loginPage.getUrl();
+        loginPage = new Login(driver);
+        loginPage.getUrl();
 
         it("username should be Maarten", function (done) {
             usernameInput = loginPage.inputUsername();
