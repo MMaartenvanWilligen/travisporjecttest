dcbb39acd7774362c61615eb1a039793b4cd53f4
Maarten van Willigen <maarten@connectholland.nl>
2016-09-30 10:11:52 +0200

    url wait 7

--- test/page-objects/loginPage.js
+++ test/page-objects/loginPage.js
@@ -3,6 +3,7 @@
  */
 var webdriver = require("selenium-webdriver");
 var Page = require("./page");
+var until = webdriver.until;
 
 /*
  * constructor HomePage object
@@ -14,7 +15,6 @@ function Login(driver) {
 
     Page.call(this, driver, "http://localhost:8000/website/loginPage.html");
     console.log("driver" + " " + driver);
-
 }
 
 // subclass Homepage extends page
@@ -24,26 +24,24 @@ Login.prototype.constructor = Login;
 
 Login.prototype.inputUsername = function () {
 
-    this.driver.wait(until.elementLocated(By.id('username')), 5000).then(function(elm) {
+    this.driver.wait(until.elementLocated(webdriver.id('username')), 5000).then(function (elm) {
         return elm;
     });
 
     //return this.driver.findElement(webdriver.By.id("username"));
-   //inputUserName.sendKeys('Maarten');
-
+    //inputUserName.sendKeys('Maarten');
 };
 
 Login.prototype.inputUsernameGetValue = function () {
 
-    this.driver.wait(until.elementLocated(By.id('username')), 5000).then(function(elm) {
+    this.driver.wait(until.elementLocated(webdriver.id('username')), 5000).then(function (elm) {
         return elm;
     });
-
 };
 
-Login.prototype.inputUsernameSetValue =  function () {
+Login.prototype.inputUsernameSetValue = function () {
 
-    driver.wait(until.elementLocated(By.name('username')), 5 * 1000).then(function(elm) {
+    driver.wait(until.elementLocated(webdriver.name('username')), 5 * 1000).then(function (elm) {
         elm.sendKeys("Admin");
     });
 };
